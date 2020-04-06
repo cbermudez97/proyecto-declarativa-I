2561a2e1e5b1827109e24d85f7b4e9c6aed7a56a
cbermudez <cbermu.97@gmail.com>
2020-04-06 18:25:31 -0400

    Implementar predicados base para la realización de jugadas de un jugador PC

---
+++ Azul/player_logic.pl
@@ -0,0 +1,32 @@
+:-[player].
+
+% Get Player First Posible Move
+getImpData((Type, Cant), (Type, Cant)) :-
+    !.
+getImpData((_, Type, Cant), (Type,Cant)) :-
+    !.
+
+buildMove(PlayerId, ((FactId, Type, Cant), Row), Move, no) :-
+    Move=(moveTile((FactId, Type, Cant), no), addTilesToRow(PlayerId, Row, Type, Cant)),
+    !.
+
+buildMove(PlayerId, ((Type, Cant), Row), Move, Especial) :-
+    Move=(moveTile((Type, Cant), Especial), addTilesToRow(PlayerId, Row, Type, Cant)),
+    !.
+
+getFirstMove(PlayerId, Move, Especial) :- 
+    getAllMoves(PossiblePlays),
+    findall(
+        (Y, Row),
+        (
+            member(Y,PossiblePlays), 
+            getImpData(Y, (Type, Cant)),
+            getPlayerRow(PlayerId, Row, ActRow),
+            player(PlayerId,_,_,_,_,_,_,_,Wall),
+            not(member((Row, _, Type), Wall)),
+            getNewRow(Row, ActRow, (Type, Cant), _, _)
+        ), 
+        [RawMove|_]
+    ),
+    buildMove(PlayerId, RawMove, Move, Especial),
+    !.
