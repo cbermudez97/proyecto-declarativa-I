252c25ecf45e067ee53f191237aafe8e10e39773
cbermudez <cbermu.97@gmail.com>
2020-04-07 12:39:25 -0400

    Terminar predicados base para el manejo del juego

--- game.pl
+++ game.pl
@@ -2,6 +2,23 @@
     "Azul/player_logic.pl"
 ].
 
+% Update next round first player
+checkEspecial(_, no).
+checkEspecial(PlayerId, si) :-
+    dropEspecial(PlayerId, si),
+    update_first(PlayerId).
+
+% Start players rotations making all moves possible
+startPlayerRotation(_) :-
+    getAllMoves([]).
+startPlayerRotation(CantPlayers) :-
+    actual_player(PlayerId),
+    playerMove(PlayerId, Move, Especial),
+    checkEspecial(PlayerId, Especial),
+    Move,
+    rotate_actual(CantPlayers),
+    !.
+
 % Number of Factories with N players
 toBuildFacts(2, 5).
 toBuildFacts(3, 7).
@@ -21,8 +38,19 @@ startAzulRound(_) :-
     ),
     !.
 startAzulRound(CantPlayers) :-
-    toBuildFacts(CantPlayers, CantFacts),
-    buildFacts(CantFacts),
+    first_player(RoundFirst), % Set round first player
+    update_actual(RoundFirst),
+    toBuildFacts(CantPlayers, CantFacts), % Build Factories
+    buildFacts(CantFacts), 
+    startPlayerRotation(CantPlayers), % All players make all moves
+    findall(_, (player(PlayerId,_,_,_,_,_,_,_,_), playerRoundEnd(PlayerId)), _), % Update players board
+    discarted(Discarted), % Rebuild bag
+    retract(bag(Bag)),
+    concatList(Bag,Discarted,NewBag),
+    assert(bag(NewBag)),
+    buildCenter, % Reset Center
+    buildDiscarted, % Reset Discarted
+    !.
 
 % Start Azul Game with N players
 startAzulGame(CantPlayers) :-
